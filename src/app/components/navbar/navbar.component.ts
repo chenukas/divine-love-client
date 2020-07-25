9cbf08b7fdcb1e8ecc862fb36719dd1d467aa7fd
Chenuka Sumanasekara <58046625+chenukas@users.noreply.github.com>
2020-07-26 02:03:33 +0530

    Initial commit

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { Router } from "@angular/router";
+import { NgxSpinnerService } from "ngx-spinner";
+
+import { AuthService } from "src/app/services/auth.service";
+
+import { User } from "src/app/models/User";
+import { Order } from "src/app/models/Order";
+
+@Component({
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
+})
+export class NavbarComponent implements OnInit, OnChanges {
+  @Input() user: User;
+  orders: Order[];
+  total: number = 0;
+  isLoading: boolean = true;
+
+  constructor (
+    private authService: AuthService, 
+    private router: Router,
+    private spinner: NgxSpinnerService
+    ) {}
+
+  ngOnInit() {
+    this.authService
+      .getCurrentUser()
+      .subscribe(res => (this.authService.userDetails(res.user), this.authService.userOrdersDetails(res.orders[0])));
+    this.authService.currentUserData.subscribe(user => (this.user = user));
+    this.authService.currentUserOrdersData.subscribe(orders => (this.orders = orders));
+    this.isLoading = false;
+    this.spinner.show();
+
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 5000);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.total = 0;
+    this.user.cart.items.forEach(item => (this.total += item.prod_total));
+  }
+
+  logout() {
+    this.authService.logoutUser();
+    this.authService.userDetails(null);
+    this.router.navigate["/"];
+  }
+}
